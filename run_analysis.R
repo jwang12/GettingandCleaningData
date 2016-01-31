d7c989e0d3392816f43fdee307333ac4a45a5132
jwang12 <jwang1277@gmail.com>
2016-01-31 01:42:45 -0500

    First commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,4 @@
 
-################################################################################################
-## COURSERA Getting and Cleaning Data project
-##
-## With this code you can go from a row data to a tidy data set with the average of choosen variables for each activity and each subject. 
-## An order of code is not exactly the same as specified in task, but the actions and the results are exactly the same. 
-## More details you can find in the CodeBook file.
-##
-## More details you can find in the ReadMe file.
-##
-################################################################################################
-
 library(reshape2)
 
 ## Download and unzip the dataset
@@ -40,21 +29,21 @@ test.data <- cbind(test.data, read.table("UCI HAR Dataset/test/subject_test.txt"
 train.data <- cbind(train.data, read.table("UCI HAR Dataset/train/subject_train.txt", col.names="Subject"))
 
 
-## merging data to one data set
+## merging dataset
 
 data.set <- rbind(test.data,train.data)
 # head(data.set,5)
 
 
 
-######### Using descriptive activity names to name the activities in the data set.
+## Using descriptive activity names to name the activities in the dataset.
 
 activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 data.set[,"Activity"] = activity.labels[data.set[,"Activity"], 2]
 
 
 
-######### Extracting only the measurements on the mean and standard deviation for each measurement. 
+## Extracting only the measurements on the mean and standard deviation for each measurement. 
 
 ## finding names of columns with mean and standard deviation.
 
@@ -66,8 +55,7 @@ choosen.column.numbers <- grep("-mean\\(\\)|-std\\(\\)", column.names)
 extracted.data.set <- data.set[,c(562,563, choosen.column.numbers)]
 # names(data.set2)
 
-
-######## Creating a second, independent tidy data set with the average of each variable for each activity and each subject. 
+## Creating a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
 molten <- melt(extracted.data.set, id.vars=c("Activity","Subject"))
 tidy.data.set <- dcast(molten, Activity + Subject ~ variable, mean)
